51fb3c5da72232a8b08881d5f4490cbe3568bcd7
Joao Paulo Nunes <jpnune@gmail.com>
2026-06-11 18:24:54 -0300

    adicionado os templates, base, index, post details e side bar

--- blog/views/post_view.py
+++ blog/views/post_view.py
@@ -1,9 +1,19 @@
+from blog.models import Post
 from django import http
 from django.http import HttpResponse
 from django.views  import generic
 
 
 
-class PostView(generic.View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('<h1>hello world</h1>')
+class PostView(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name= "index.html"
+        
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = "post_detail.html"
+    
+    
+    
+    
