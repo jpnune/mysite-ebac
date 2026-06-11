b65b629ddb9024642dfd0ee529e751cdf994341d
Joao Paulo Nunes <jpnune@gmail.com>
2026-06-11 16:20:41 -0300

    Remover env e limpar dependencies do requirements.txt

---
+++ blog/views/post_view.py
@@ -0,0 +1,9 @@
+from django import http
+from django.http import HttpResponse
+from django.views  import generic
+
+
+
+class PostView(generic.View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('<h1>hello world</h1>')
